3688e94d325215241fd6b3d753591a4e36b60342
darylhjd <aka.hjd@gmail.com>
2023-04-27 01:07:00 +0200

    feat: add test for apiserver ping handler

---
+++ backend/servers/apiserver/v1/ping_test.go
@@ -0,0 +1,34 @@
+package v1
+
+import (
+	"io"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAPIServerV1_ping(t *testing.T) {
+	server := newTestAPIServerV1()
+	defer server.Close()
+
+	reqUrl, err := url.JoinPath(server.URL, "/ping")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resp, err := http.Get(reqUrl)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	a := assert.New(t)
+	a.Equal(http.StatusOK, resp.StatusCode)
+	a.Contains(string(body), "Pong~")
+}
