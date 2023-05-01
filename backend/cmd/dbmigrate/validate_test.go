d574508cbf7e53952b68b62251597f337fe7f63e
darylhjd <aka.hjd@gmail.com>
2023-05-01 14:24:08 +0200

    fix: revert test

--- backend/cmd/dbmigrate/validate_test.go
+++ backend/cmd/dbmigrate/validate_test.go
@@ -160,7 +160,10 @@ func Test_validateArguments(t *testing.T) {
 	a := assert.New(t)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := validateArguments(&(tt.args)); tt.wantErr {
+			err := validateArguments(&(tt.args))
+			a.Equal(tt.wantErr, err != nil)
+
+			if tt.wantErr {
 				a.ErrorContains(err, tt.containsErr)
 			}
 		})
