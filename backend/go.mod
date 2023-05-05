6e570068a029e6f440e14b1ae8d2abeb5257ff26
darylhjd <aka.hjd@gmail.com>
2023-05-05 13:18:07 +0200

    feat: use azure ad for getting authorization code

--- backend/go.mod
+++ backend/go.mod
@@ -3,6 +3,7 @@ module github.com/darylhjd/oams/backend
 go 1.20
 
 require (
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.0.0
 	github.com/darylhjd/oams/frontend v0.0.0-20230503222224-ddcfa83135c4
 	github.com/golang-migrate/migrate/v4 v4.15.2
 	github.com/joho/godotenv v1.5.1
@@ -13,10 +14,15 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/servers/apiserver/v1/v1.go
+++ backend/servers/apiserver/v1/v1.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"net/http"
 
+	"github.com/AzureAD/microsoft-authentication-library-for-go/apps/confidential"
 	"go.uber.org/zap"
 
 	"github.com/darylhjd/oams/backend/database"
@@ -12,13 +13,17 @@ import (
 const (
 	namespace = "apiserver/v1"
 
-	baseUrl = "/"
-	pingUrl = "/ping"
+	baseUrl            = "/"
+	pingUrl            = "/ping"
+	loginUrl           = "/login"
+	msLoginCallbackUrl = "/ms-login-callback"
 )
 
 type APIServerV1 struct {
 	l  *zap.Logger
 	db *database.DB
+
+	azure *confidential.Client
 }
 
 func (v *APIServerV1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -26,12 +31,14 @@ func (v *APIServerV1) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	mux.HandleFunc(baseUrl, servers.AllowMethods(v.base, http.MethodGet))
 	mux.HandleFunc(pingUrl, servers.AllowMethods(v.ping, http.MethodGet))
+	mux.HandleFunc(loginUrl, v.login)
+	mux.HandleFunc(msLoginCallbackUrl, servers.AllowMethods(v.msLoginCallback, http.MethodPost))
 
 	mux.ServeHTTP(w, r)
 }
 
 // NewAPIServerV1 creates a new APIServerV1. This is a sub-router and should not be used
 // as a base router.
-func NewAPIServerV1(l *zap.Logger, db *database.DB) *APIServerV1 {
-	return &APIServerV1{l, db}
+func NewAPIServerV1(l *zap.Logger, db *database.DB, azureClient *confidential.Client) *APIServerV1 {
+	return &APIServerV1{l, db, azureClient}
 }
