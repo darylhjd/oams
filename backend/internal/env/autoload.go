be3c20825ab046957d5c166d6b5885275ac06c0b
darylhjd <aka.hjd@gmail.com>
2023-05-17 22:54:29 +0200

    hotfix: remove test logging

--- backend/internal/env/autoload.go
+++ backend/internal/env/autoload.go
@@ -1,10 +1,7 @@
 package env
 
-import "log"
-
 // init helps to automatically load the environment variables for a programme.
 // This should be the only init function in the codebase!!!
 func init() {
 	MustLoad()
-	log.Println("Loaded envs")
 }
