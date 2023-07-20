a3bf8838824926f7220b7a252f3a875439b20070
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-07-20 15:10:34 +0800

    feat: implement parser for class creation file (#47)

--- backend/go.mod
+++ backend/go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/rs/cors v1.9.0
 	github.com/stretchr/testify v1.8.4
+	github.com/xuri/excelize/v2 v2.7.1
 	go.uber.org/zap v1.24.0
 )
 
@@ -29,11 +30,18 @@ require (
 	github.com/lestrrat-go/httprc v1.0.4 // indirect
 	github.com/lestrrat-go/iter v1.0.2 // indirect
 	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.3 // indirect
 	github.com/segmentio/asm v1.2.0 // indirect
+	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.10.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ backend/servers/apiserver/common/class_creation_parser_test.go
@@ -0,0 +1,209 @@
+package common
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/darylhjd/oams/backend/internal/database"
+)
+
+func TestParseClassCreationFile(t *testing.T) {
+	loc, err := time.LoadLocation(timezoneLocation)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tests := []struct {
+		name         string
+		file         string
+		containsErr  string
+		expectedData ClassCreationData
+	}{
+		{
+			"sample class lab creation file",
+			"class_lab_test.xlsx",
+			"",
+			ClassCreationData{
+				2022,
+				"2",
+				time.Date(2023, time.June, 15, 13, 1, 0, 0, loc),
+				"SC1015",
+				"LAB",
+				[]ClassGroup{
+					{
+						"A21",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"CHUL6789", "CHUA LI TING", sql.NullString{}},
+							{"YAPW9087", "YAP WEN LI", sql.NullString{}},
+						},
+					},
+					{
+						"A26",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"BENST129", "BENJAMIN SANTOS", sql.NullString{}},
+							{"YAPW9087", "YAP WEI LING", sql.NullString{}},
+						},
+					},
+					{
+						"A32",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"PATELAR14", "ARJUN PATEL", sql.NullString{}},
+							{"YAPX9087", "YAP XIN TING", sql.NullString{}},
+						},
+					},
+				},
+			},
+		},
+		{
+			"sample class lecture creation file",
+			"class_lec_test.xlsx",
+			"",
+			ClassCreationData{
+				2022,
+				"2",
+				time.Date(2023, time.June, 15, 13, 1, 0, 0, loc),
+				"SC1015",
+				"LEC",
+				[]ClassGroup{
+					{
+						"L1",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"PATELAR14", "ARJUN PATEL", sql.NullString{}},
+							{"YAPX9087", "YAP XIN TING", sql.NullString{}},
+						},
+					},
+				},
+			},
+		},
+		{
+			"sample class tutorial creation file",
+			"class_tut_test.xlsx",
+			"",
+			ClassCreationData{
+				2022,
+				"2",
+				time.Date(2023, time.June, 15, 13, 1, 0, 0, loc),
+				"SC1015",
+				"TUT",
+				[]ClassGroup{
+					{
+						"A21",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"CHUL6789", "CHUA LI TING", sql.NullString{}},
+							{"YAPW9087", "YAP WEN LI", sql.NullString{}},
+						},
+					},
+					{
+						"A26",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"BENST129", "BENJAMIN SANTOS", sql.NullString{}},
+							{"YAPW9087", "YAP WEI LING", sql.NullString{}},
+						},
+					},
+					{
+						"A32",
+						[]database.ClassVenueSchedule{},
+						[]database.Student{
+							{"PATELAR14", "ARJUN PATEL", sql.NullString{}},
+							{"YAPX9087", "YAP XIN TING", sql.NullString{}},
+						},
+					},
+				},
+			},
+		},
+		{
+			"empty class creation file",
+			"class_empty_test.xlsx",
+			"not enough rows for class metadata",
+			ClassCreationData{},
+		},
+		{
+			"too many class metadata rows",
+			"class_excessive_class_metadata_rows.xlsx",
+			"unexpected number of columns for class group row",
+			ClassCreationData{},
+		},
+		{
+			"missing class group enrollment list identifier",
+			"class_missing_enrollment_ident.xlsx",
+			"unexpected start of class group enrollment list",
+			ClassCreationData{},
+		},
+		{
+			"second class group missing enrollment list identifier",
+			"class_second_group_missing_enrollment_ident.xlsx",
+			"unexpected start of class group enrollment list",
+			ClassCreationData{},
+		},
+		{
+			"student row with wrong length",
+			"class_student_row_wrong_length.xlsx",
+			"unexpected number of columns for student enrollment row",
+			ClassCreationData{},
+		},
+		{
+			"class group with no enrollment",
+			"class_group_with_no_enrollment.xlsx",
+			"class group A21 has no enrollments",
+			ClassCreationData{},
+		},
+		{
+			"course with no class groups",
+			"class_with_no_groups.xlsx",
+			"class creation file has no valid class groups",
+			ClassCreationData{},
+		},
+		{
+			"invalid format for class group name",
+			"class_with_invalid_class_group_name.xlsx",
+			"could not parse class group",
+			ClassCreationData{},
+		},
+	}
+
+	a := assert.New(t)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			file, err := os.Open(tt.file)
+			a.Nil(err)
+
+			data, err := ParseClassCreationFile(file)
+			if tt.containsErr != "" {
+				a.ErrorContains(err, tt.containsErr)
+				return
+			}
+
+			attributeTests := map[any]any{
+				tt.expectedData.Year:             data.Year,
+				tt.expectedData.Semester:         data.Semester,
+				tt.expectedData.FileCreationDate: data.FileCreationDate,
+				tt.expectedData.CourseCode:       data.CourseCode,
+				tt.expectedData.ClassType:        data.ClassType,
+			}
+
+			for expected, actual := range attributeTests {
+				a.Equal(expected, actual)
+			}
+
+			// Check class groups.
+			a.Equal(len(tt.expectedData.ClassGroups), len(data.ClassGroups))
+			for i := 0; i < len(tt.expectedData.ClassGroups); i++ {
+				a.Equal(tt.expectedData.ClassGroups[i].Name, data.ClassGroups[i].Name)
+				// TODO: Test for schedule.
+				for _, student := range tt.expectedData.ClassGroups[i].Students {
+					a.Contains(data.ClassGroups[i].Students, student)
+				}
+			}
+		})
+	}
+}
