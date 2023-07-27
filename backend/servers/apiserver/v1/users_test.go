9177f61376f429cf7290d75fdc6d4e679e8ae18f
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-07-27 11:40:49 +0800

    refactor: update request and response fields for user and users endpoints (#71)

--- backend/servers/apiserver/v1/users_test.go
+++ backend/servers/apiserver/v1/users_test.go
@@ -11,7 +11,6 @@ import (
 	"github.com/darylhjd/oams/backend/internal/database"
 	"github.com/darylhjd/oams/backend/internal/tests"
 	"github.com/google/uuid"
-	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -64,22 +63,20 @@ func TestAPIServerV1_usersGet(t *testing.T) {
 	t.Parallel()
 
 	tts := []struct {
-		name                    string
-		withStubAuthContextUser bool
-		wantResponse            usersGetResponse
+		name             string
+		withExistingUser bool
+		wantResponse     usersGetResponse
 	}{
 		{
 			"request with user in database",
 			true,
 			usersGetResponse{
 				newSuccessResponse(),
-				[]database.User{
+				[]usersGetUserResponseFields{
 					{
-						ID: tests.MockAuthenticatorIDTokenName,
-						Email: pgtype.Text{
-							String: tests.MockAuthenticatorAccountPreferredUsername,
-							Valid:  true,
-						},
+						ID:    "EXISTING_USER",
+						Email: nil,
+						Role:  database.UserRoleSTUDENT,
 					},
 				},
 			},
@@ -89,7 +86,7 @@ func TestAPIServerV1_usersGet(t *testing.T) {
 			false,
 			usersGetResponse{
 				response: newSuccessResponse(),
-				Users:    []database.User{},
+				Users:    []usersGetUserResponseFields{},
 			},
 		},
 	}
@@ -106,8 +103,10 @@ func TestAPIServerV1_usersGet(t *testing.T) {
 			v1 := newTestAPIServerV1(t, id)
 			defer tests.TearDown(t, v1.db, id)
 
-			if tt.withStubAuthContextUser {
-				tests.StubAuthContextUser(t, ctx, v1.db.Q)
+			if tt.withExistingUser {
+				for _, user := range tt.wantResponse.Users {
+					tests.StubUser(t, ctx, v1.db.Q, user.ID)
+				}
 			}
 
 			req := httptest.NewRequest(http.MethodGet, usersUrl, nil)
@@ -115,6 +114,11 @@ func TestAPIServerV1_usersGet(t *testing.T) {
 			a.True(ok)
 
 			a.Equal(len(tt.wantResponse.Users), len(actualResp.Users))
+			for idx, actualUser := range actualResp.Users {
+				tt.wantResponse.Users[idx].CreatedAt, tt.wantResponse.Users[idx].UpdatedAt = actualUser.CreatedAt, actualUser.UpdatedAt
+			}
+
+			a.Equal(tt.wantResponse, actualResp)
 		})
 	}
 }
@@ -123,20 +127,42 @@ func TestAPIServerV1_usersPost(t *testing.T) {
 	t.Parallel()
 
 	tts := []struct {
-		name                    string
-		withStubAuthContextUser bool
-		wantStatusCode          int
-		wantErr                 string
+		name             string
+		withRequest      usersCreateRequest
+		withExistingUser bool
+		wantResponse     usersCreateResponse
+		wantStatusCode   int
+		wantErr          string
 	}{
 		{
 			"request with no existing user",
+			usersCreateRequest{
+				usersCreateUserRequestFields{
+					ID:   "NEW_USER",
+					Role: database.UserRoleSTUDENT,
+				},
+			},
 			false,
+			usersCreateResponse{
+				newSuccessResponse(),
+				usersCreateUserResponseFields{
+					ID:   "NEW_USER",
+					Role: database.UserRoleSTUDENT,
+				},
+			},
 			http.StatusOK,
 			"",
 		},
 		{
 			"request with existing user",
+			usersCreateRequest{
+				usersCreateUserRequestFields{
+					ID:   "EXISTING_USER",
+					Role: database.UserRoleSTUDENT,
+				},
+			},
 			true,
+			usersCreateResponse{},
 			http.StatusConflict,
 			"user with same id already exists",
 		},
@@ -154,24 +180,16 @@ func TestAPIServerV1_usersPost(t *testing.T) {
 			v1 := newTestAPIServerV1(t, id)
 			defer tests.TearDown(t, v1.db, id)
 
-			if tt.withStubAuthContextUser {
-				tests.StubAuthContextUser(t, ctx, v1.db.Q)
+			if tt.withExistingUser {
+				tests.StubUser(t, ctx, v1.db.Q, tt.withRequest.User.ID)
 			}
 
-			reqBodyBytes, err := json.Marshal(usersCreateRequest{
-				database.CreateUserParams{
-					ID: tests.MockAuthenticatorIDTokenName,
-					Email: pgtype.Text{
-						String: tests.MockAuthenticatorAccountPreferredUsername,
-						Valid:  true,
-					},
-					Role: database.UserRoleSTUDENT,
-				},
-			})
+			reqBodyBytes, err := json.Marshal(tt.withRequest)
 			a.Nil(err)
 
 			req := httptest.NewRequest(http.MethodPost, usersUrl, bytes.NewReader(reqBodyBytes))
 			resp := v1.usersCreate(req)
+			a.Equal(tt.wantStatusCode, resp.Code())
 
 			switch {
 			case tt.wantErr != "":
@@ -181,7 +199,9 @@ func TestAPIServerV1_usersPost(t *testing.T) {
 			default:
 				actualResp, ok := resp.(usersCreateResponse)
 				a.True(ok)
-				a.Equal(tests.MockAuthenticatorIDTokenName, actualResp.User.ID)
+
+				tt.wantResponse.User.CreatedAt = actualResp.User.CreatedAt
+				a.Equal(tt.wantResponse, actualResp)
 			}
 		})
 	}
