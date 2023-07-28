77473c271a7f2f460108c4fc460ee4ac26b19b49
Daryl <aka.hjd@gmail.com>
2023-07-28 10:55:47 +0800

    feat: implement class get endpoint

--- backend/servers/apiserver/v1/class.go
+++ backend/servers/apiserver/v1/class.go
@@ -1,25 +1,55 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 	"strings"
+
+	"github.com/darylhjd/oams/backend/internal/database"
+	"github.com/jackc/pgx/v5"
 )
 
 func (v *APIServerV1) class(w http.ResponseWriter, r *http.Request) {
 	var resp apiResponse
 
-	_ = strings.TrimPrefix(r.URL.Path, classUrl)
+	classId, err := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, classUrl), 10, 64)
+	if err != nil {
+		v.writeResponse(w, classUrl, newErrorResponse(http.StatusUnprocessableEntity, "invalid class id"))
+		return
+	}
+
 	switch r.Method {
 	case http.MethodGet:
-		resp = newErrorResponse(http.StatusNotImplemented, "")
+		resp = v.classGet(r, classId)
 	case http.MethodPut:
 		resp = newErrorResponse(http.StatusNotImplemented, "")
 	case http.MethodDelete:
 		resp = newErrorResponse(http.StatusNotImplemented, "")
 	default:
 		resp = newErrorResponse(http.StatusMethodNotAllowed, "")
-
 	}
 
 	v.writeResponse(w, classUrl, resp)
+}
+
+type classGetResponse struct {
+	response
+	Class database.Class `json:"class"`
+}
+
+func (v *APIServerV1) classGet(r *http.Request, id int64) apiResponse {
+	class, err := v.db.Q.GetClass(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return newErrorResponse(http.StatusNotFound, "the requested class does not exist")
+		}
+
+		return newErrorResponse(http.StatusInternalServerError, "could not process class get database action")
+	}
+
+	return classGetResponse{
+		newSuccessResponse(),
+		class,
+	}
 }
--- backend/servers/apiserver/v1/class_test.go
+++ backend/servers/apiserver/v1/class_test.go
@@ -1,10 +1,13 @@
 package v1
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/darylhjd/oams/backend/internal/database"
 	"github.com/darylhjd/oams/backend/internal/tests"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -21,7 +24,7 @@ func TestAPIServerV1_class(t *testing.T) {
 		{
 			"with GET method",
 			http.MethodGet,
-			http.StatusNotImplemented,
+			http.StatusNotFound,
 		},
 		{
 			"with PUT method",
@@ -51,11 +54,87 @@ func TestAPIServerV1_class(t *testing.T) {
 			v1 := newTestAPIServerV1(t, id)
 			defer tests.TearDown(t, v1.db, id)
 
-			req := httptest.NewRequest(tt.withMethod, classUrl, nil)
+			req := httptest.NewRequest(tt.withMethod, fmt.Sprintf("%s%d", classUrl, 1), nil)
 			rr := httptest.NewRecorder()
 			v1.class(rr, req)
 
 			a.Equal(tt.wantStatusCode, rr.Code)
+		})
+	}
+}
+
+func TestAPIServerV1_classGet(t *testing.T) {
+	t.Parallel()
+
+	tts := []struct {
+		name              string
+		withExistingClass bool
+		wantResponse      classGetResponse
+		wantStatusCode    int
+		wantErr           string
+	}{
+		{
+			"request with class in database",
+			true,
+			classGetResponse{
+				newSuccessResponse(),
+				database.Class{
+					Code:     "CZ3454",
+					Year:     2023,
+					Semester: "1",
+				},
+			},
+			http.StatusOK,
+			"",
+		},
+		{
+			"request with class not in database",
+			false,
+			classGetResponse{},
+			http.StatusNotFound,
+			"the requested class does not exist",
+		},
+	}
+
+	for _, tt := range tts {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			a := assert.New(t)
+			ctx := context.Background()
+			id := uuid.NewString()
+
+			v1 := newTestAPIServerV1(t, id)
+			defer tests.TearDown(t, v1.db, id)
+
+			if tt.withExistingClass {
+				createdClass := tests.StubClass(
+					t, ctx, v1.db.Q,
+					tt.wantResponse.Class.Code,
+					tt.wantResponse.Class.Year,
+					tt.wantResponse.Class.Semester,
+				)
+
+				tt.wantResponse.Class.ID = createdClass.ID
+				tt.wantResponse.Class.CreatedAt = createdClass.CreatedAt
+				tt.wantResponse.Class.UpdatedAt = createdClass.CreatedAt
+			}
+
+			req := httptest.NewRequest(http.MethodGet, fmt.Sprintf("%s%d", userUrl, tt.wantResponse.Class.ID), nil)
+			resp := v1.classGet(req, tt.wantResponse.Class.ID)
+			a.Equal(tt.wantStatusCode, resp.Code())
+
+			switch {
+			case tt.wantErr != "":
+				actualResp, ok := resp.(errorResponse)
+				a.True(ok)
+				a.Contains(actualResp.Error, tt.wantErr)
+			default:
+				actualResp, ok := resp.(classGetResponse)
+				a.True(ok)
+				a.Equal(tt.wantResponse, actualResp)
+			}
 		})
 	}
 }
