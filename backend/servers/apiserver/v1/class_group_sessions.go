9d48ba9afa808dbad54f014b26d762df66a77c66
Daryl <aka.hjd@gmail.com>
2023-08-01 17:03:47 +0800

    feat: implement class group sessions get

--- backend/servers/apiserver/v1/class_group_sessions.go
+++ backend/servers/apiserver/v1/class_group_sessions.go
@@ -1,13 +1,17 @@
 package v1
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/darylhjd/oams/backend/internal/database"
+)
 
 func (v *APIServerV1) classGroupSessions(w http.ResponseWriter, r *http.Request) {
 	var resp apiResponse
 
 	switch r.Method {
 	case http.MethodGet:
-		resp = newErrorResponse(http.StatusNotImplemented, "")
+		resp = v.classGroupSessionsGet(r)
 	case http.MethodPost:
 		resp = newErrorResponse(http.StatusNotImplemented, "")
 	default:
@@ -15,4 +19,24 @@ func (v *APIServerV1) classGroupSessions(w http.ResponseWriter, r *http.Request)
 	}
 
 	v.writeResponse(w, classGroupSessionsUrl, resp)
+}
+
+type classGroupSessionsGetResponse struct {
+	response
+	ClassGroupSessions []database.ClassGroupSession `json:"class_group_sessions"`
+}
+
+func (v *APIServerV1) classGroupSessionsGet(r *http.Request) apiResponse {
+	sessions, err := v.db.Q.ListClassGroupSessions(r.Context())
+	if err != nil {
+		return newErrorResponse(http.StatusInternalServerError, "could not process class group sessions get database action")
+	}
+
+	resp := classGroupSessionsGetResponse{
+		newSuccessResponse(),
+		make([]database.ClassGroupSession, 0, len(sessions)),
+	}
+
+	resp.ClassGroupSessions = append(resp.ClassGroupSessions, sessions...)
+	return resp
 }
