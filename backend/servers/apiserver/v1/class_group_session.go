0a772aba706a5bbabfc985850943f4d653ed5589
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-08-02 21:18:11 +0800

    feat: implement class group session get endpoint (#106)

--- backend/servers/apiserver/v1/class_group_session.go
+++ backend/servers/apiserver/v1/class_group_session.go
@@ -1,15 +1,19 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/darylhjd/oams/backend/internal/database"
+	"github.com/jackc/pgx/v5"
 )
 
 func (v *APIServerV1) classGroupSession(w http.ResponseWriter, r *http.Request) {
 	var resp apiResponse
 
-	_, err := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, classGroupSessionUrl), 10, 64)
+	sessionId, err := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, classGroupSessionUrl), 10, 64)
 	if err != nil {
 		v.writeResponse(w, classGroupSessionUrl, newErrorResponse(http.StatusUnprocessableEntity, "invalid class group session id"))
 		return
@@ -17,7 +21,7 @@ func (v *APIServerV1) classGroupSession(w http.ResponseWriter, r *http.Request) 
 
 	switch r.Method {
 	case http.MethodGet:
-		resp = newErrorResponse(http.StatusNotImplemented, "")
+		resp = v.classGroupSessionGet(r, sessionId)
 	case http.MethodPut:
 		resp = newErrorResponse(http.StatusNotImplemented, "")
 	case http.MethodDelete:
@@ -27,4 +31,23 @@ func (v *APIServerV1) classGroupSession(w http.ResponseWriter, r *http.Request) 
 	}
 
 	v.writeResponse(w, classGroupSessionUrl, resp)
+}
+
+type classGroupSessionGetResponse struct {
+	response
+	ClassGroupSession database.ClassGroupSession `json:"class_group_session"`
+}
+
+func (v *APIServerV1) classGroupSessionGet(r *http.Request, id int64) apiResponse {
+	session, err := v.db.Q.GetClassGroupSession(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return newErrorResponse(http.StatusNotFound, "the requested class group session does not exist")
+		}
+	}
+
+	return classGroupSessionGetResponse{
+		newSuccessResponse(),
+		session,
+	}
 }
