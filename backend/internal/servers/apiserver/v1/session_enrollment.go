5a48d12c1399e4af11dc54c8d7957565b3345258
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-08-03 22:12:13 +0800

    feat: implement session enrollment get endpoint (#120)

--- backend/internal/servers/apiserver/v1/session_enrollment.go
+++ backend/internal/servers/apiserver/v1/session_enrollment.go
@@ -1,22 +1,26 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 	"strings"
+
+	"github.com/darylhjd/oams/backend/internal/database"
+	"github.com/jackc/pgx/v5"
 )
 
 func (v *APIServerV1) sessionEnrollment(w http.ResponseWriter, r *http.Request) {
 	var resp apiResponse
 
-	_, err := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, sessionEnrollmentUrl), 10, 64)
+	enrollmentId, err := strconv.ParseInt(strings.TrimPrefix(r.URL.Path, sessionEnrollmentUrl), 10, 64)
 	if err != nil {
 		v.writeResponse(w, sessionEnrollmentUrl, newErrorResponse(http.StatusUnprocessableEntity, "invalid session_enrollment id"))
 	}
 
 	switch r.Method {
 	case http.MethodGet:
-		resp = newErrorResponse(http.StatusNotImplemented, "")
+		resp = v.sessionEnrollmentGet(r, enrollmentId)
 	case http.MethodPatch:
 		resp = newErrorResponse(http.StatusNotImplemented, "")
 	case http.MethodDelete:
@@ -26,4 +30,25 @@ func (v *APIServerV1) sessionEnrollment(w http.ResponseWriter, r *http.Request) 
 	}
 
 	v.writeResponse(w, sessionEnrollmentUrl, resp)
+}
+
+type sessionEnrollmentGetResponse struct {
+	response
+	SessionEnrollment database.SessionEnrollment `json:"session_enrollment"`
+}
+
+func (v *APIServerV1) sessionEnrollmentGet(r *http.Request, id int64) apiResponse {
+	enrollment, err := v.db.Q.GetSessionEnrollment(r.Context(), id)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return newErrorResponse(http.StatusNotFound, "the requested session enrollment does not exist")
+		}
+
+		return newErrorResponse(http.StatusInternalServerError, "could not process session enrollment get database action")
+	}
+
+	return sessionEnrollmentGetResponse{
+		newSuccessResponse(),
+		enrollment,
+	}
 }
