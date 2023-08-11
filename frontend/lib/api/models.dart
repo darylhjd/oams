ff6e75000117021b63a94cf5071667eaddcb352f
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-08-11 08:53:29 +0800

    fix: change frontend to support user me upcoming sessions (#136)

--- frontend/lib/api/models.dart
+++ frontend/lib/api/models.dart
@@ -14,12 +14,15 @@ class LoginResponse {
 }
 
 class UserMeResponse {
-  final User? sessionUser;
+  final User sessionUser;
+  final List<UpcomingClassGroupSession> upcomingSessions;
 
   UserMeResponse.fromJson(Map<String, dynamic> json)
-      : sessionUser = json["session_user"] == null
-            ? null
-            : User.fromJson(json["session_user"]);
+      : sessionUser = User.fromJson(json["session_user"]),
+        upcomingSessions = List<UpcomingClassGroupSession>.from(
+          (json["upcoming_class_group_sessions"] as List)
+              .map((i) => UpcomingClassGroupSession.fromJson(i)),
+        );
 }
 
 class User {
@@ -33,6 +36,39 @@ class User {
         name = json["name"],
         email = json["email"],
         role = json["role"];
+}
+
+class UpcomingClassGroupSession {
+  final String code;
+  final int year;
+  final String semester;
+  final String name;
+  final ClassType classType;
+  final DateTime startTime;
+  final DateTime endTime;
+
+  UpcomingClassGroupSession.fromJson(Map<String, dynamic> json)
+      : code = json["code"],
+        year = json["year"],
+        semester = json["semester"],
+        name = json["name"],
+        classType = ClassType.fromValue(json["class_type"]),
+        startTime = DateTime.parse(json["start_time"]).toLocal(),
+        endTime = DateTime.parse(json["end_time"]).toLocal();
+}
+
+enum ClassType {
+  lec("LEC"),
+  tut("TUT"),
+  lab("LAB");
+
+  final String name;
+
+  const ClassType(this.name);
+
+  factory ClassType.fromValue(String value) {
+    return ClassType.values.firstWhere((e) => e.name == value);
+  }
 }
 
 class GetUserResponse {
--- frontend/lib/providers/session.dart
+++ frontend/lib/providers/session.dart
@@ -5,14 +5,11 @@ import 'package:frontend/api/models.dart';
 // Provides information about the current session use. We do this since the API
 // uses a HttpOnly cookie to store the required auth codes and so we cannot read
 // it directly.
-final sessionUserProvider = FutureProvider<User>((ref) async {
+final sessionUserProvider = FutureProvider<UserMeResponse>((ref) async {
   try {
     var resp = await APIClient.getUserMe();
-    if (resp.sessionUser == null) {
-      throw Exception("no session user");
-    }
     ref.read(sessionProvider.notifier).update((_) => true);
-    return resp.sessionUser!;
+    return resp;
   } catch (e) {
     ref.read(sessionProvider.notifier).update((_) => false);
     rethrow;
