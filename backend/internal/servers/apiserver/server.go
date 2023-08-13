62ebc14f67af2e413079acb154c2c3fd2863a929
darylhjd <aka.hjd@gmail.com>
2023-08-13 17:17:34 +0800

    argc

--- backend/internal/servers/apiserver/server.go
+++ backend/internal/servers/apiserver/server.go
@@ -58,8 +58,8 @@ func (s *APIServer) Start() error {
 	// Set up CORS.
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{fmt.Sprintf("%s:%s", env.GetWebServerHost(), env.GetWebServerPort())},
-		AllowedHeaders:   []string{"*"},
 		AllowCredentials: true,
+		Debug:            true,
 	})
 
 	port := env.GetAPIServerPort()
