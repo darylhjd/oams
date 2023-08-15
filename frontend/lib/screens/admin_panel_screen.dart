fb63ca5433833822d161df0cdaf77890ba84d457
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-08-15 12:55:31 +0800

    feat: add placeholder admin panel screen (#148)

---
+++ frontend/lib/screens/admin_panel_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/screen_template.dart';
+
+class AdminPanelScreen extends StatelessWidget {
+  const AdminPanelScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const ScreenTemplate(
+      Placeholder(),
+    );
+  }
+}
