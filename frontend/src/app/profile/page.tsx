73ccaa9f70db61cbd633f747742de15e0efa3f7b
darylhjd <aka.hjd@gmail.com>
2023-08-27 17:56:23 +0800

    profile page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,14 +1,62 @@
 "use client";
 
 import { redirectIfNotLoggedIn } from "@/routes/checks";
-import { Center } from "@mantine/core";
+import { sessionStore } from "@/states/session";
+import { Card, Center, Container, Stack, TextInput, Title, createStyles } from "@mantine/core";
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    width: "100%",
+  }
+}))
 
 export default function LoginPage() {
   redirectIfNotLoggedIn();
 
   return (
-    <Center>
-      <p>This is the profile screen.</p>
-    </Center>
+    <Stack align="center">
+        <Header />
+        <Information />        
+    </Stack>
   );
+}
+
+function Header() {
+  return (
+    <Title>Your Profile</Title>
+  )
+}
+
+function Information() {
+  const session = sessionStore()
+  const { classes } = useStyles()
+  
+  return (
+    <Card className={classes.card} shadow="md" withBorder>
+      <Container size="30em">
+        <Stack>
+          <TextInput
+            placeholder={session.userMe!.session_user.id}
+            label="ID"
+            disabled
+          />
+          <TextInput 
+            placeholder={session.userMe!.session_user.name}
+            label="Name"
+            disabled
+          />
+          <TextInput
+            placeholder={session.userMe!.session_user.email}
+            label="Email"
+            disabled
+          />
+          <TextInput
+            placeholder={session.userMe!.session_user.role}
+            label="Role"
+            disabled
+          />
+        </Stack>        
+      </Container>
+    </Card>
+  )
 }
