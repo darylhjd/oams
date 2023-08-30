a7e2e00c9e7b59e487a13acd8fb6fd01f4894b7f
Daryl <aka.hjd@gmail.com>
2023-08-30 11:32:51 +0800

    prettier

--- frontend/src/app/guest_page.tsx
+++ frontend/src/app/guest_page.tsx
@@ -6,23 +6,23 @@ import { useMediaQuery } from "@mantine/hooks";
 const useStyles = createStyles((theme) => ({
   hero: {
     padding: "3em 0",
-  }
-}))
+  },
+}));
 
 export default function GuestHomePage() {
-  return (
-    <TopBanner />
-  );
+  return <TopBanner />;
 }
 
 function TopBanner() {
-  const {classes} = useStyles()
+  const { classes } = useStyles();
 
-  const largeScreen = useMediaQuery("(min-width: 62em)")
+  const largeScreen = useMediaQuery("(min-width: 62em)");
 
   return (
     <div className={classes.hero}>
-      <Title order={largeScreen ? 1 : 2} ta="center">Welcome to the Online Attendance Management System</Title>
+      <Title order={largeScreen ? 1 : 2} ta="center">
+        Welcome to the Online Attendance Management System
+      </Title>
     </div>
-  )
+  );
 }
