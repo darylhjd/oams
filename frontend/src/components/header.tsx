4c7b24eeb1ab29acff1bfd82c6df36c5accd025b
Daryl <aka.hjd@gmail.com>
2023-08-30 12:57:08 +0800

    save

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -17,7 +17,7 @@ import {
   IconMenu2,
   IconUserCircle,
 } from "@tabler/icons-react";
-import { Desktop, Mobile } from "./responsive";
+import { isMobile } from "./responsive";
 import { useRouter } from "next/navigation";
 import { Routes } from "@/routes/routes";
 import { sessionStore } from "@/states/session";
@@ -63,6 +63,10 @@ const useStyles = createStyles((theme) => ({
   },
 }));
 
+const mobileProp = {
+  mobile: false,
+};
+
 // Header stores the navigation bar and shows a horizontal divider bottom border.
 export default function Header() {
   const { classes } = useStyles();
@@ -106,217 +110,208 @@ function Logo() {
 }
 
 function Options() {
-  const router = useRouter();
   const session = sessionStore();
   const { classes } = useStyles();
 
+  if (isMobile()) {
+    return (
+      <Menu position="bottom-end" width={150}>
+        <Menu.Target>
+          <Button leftIcon={<IconMenu2 />} variant="subtle">
+            Menu
+          </Button>
+        </Menu.Target>
+
+        <Menu.Dropdown>
+          <AboutButton mobile />
+          {session.data != null &&
+          session.data.session_user.role == UserRole.SystemAdmin ? (
+            <>
+              <Menu.Label>Admin Controls</Menu.Label>
+              <AdminPanelButton mobile />
+              <BatchProcessingButton mobile />
+            </>
+          ) : null}
+          {session.data == null ? (
+            <LoginButton mobile />
+          ) : (
+            <>
+              <Menu.Label>Account</Menu.Label>
+              <ProfileButton mobile />
+              <LogoutButton />
+            </>
+          )}
+        </Menu.Dropdown>
+      </Menu>
+    );
+  }
+
   return (
-    <>
-      <Mobile>
-        <Menu position="bottom-end" width={150}>
-          <Menu.Target>
-            <Button leftIcon={<IconMenu2 />} variant="subtle">
-              Menu
-            </Button>
-          </Menu.Target>
-
-          <Menu.Dropdown>
-            <Menu.Item onClick={() => router.push(Routes.about)}>
-              <AboutButton />
-            </Menu.Item>
-            {session.data != null &&
-            session.data.session_user.role == UserRole.SystemAdmin ? (
-              <>
-                <Menu.Label>Admin Controls</Menu.Label>
-                <Menu.Item onClick={() => router.push(Routes.adminPanel)}>
-                  <AdminPanelButton />
-                </Menu.Item>
-                <Menu.Item onClick={() => router.push(Routes.batchProcessing)}>
-                  <BatchProcessingButton />
-                </Menu.Item>
-              </>
-            ) : null}
-            {session.data == null ? (
-              <Menu.Item
-                icon={<IconLogin stroke={1} />}
-                onClick={() => router.push(Routes.login)}
-              >
-                <LoginButton />
-              </Menu.Item>
-            ) : (
-              <>
-                <Menu.Label>Account</Menu.Label>
-                <Menu.Item
-                  icon={<IconUserCircle stroke={1} />}
-                  onClick={() => router.push(Routes.profile)}
-                >
-                  <ProfileButton />
-                </Menu.Item>
-                <Menu.Item
-                  icon={<IconLogout stroke={1} />}
-                  onClick={async () => {
-                    session.invalidate();
-                    await APIClient.logout();
-                    location.href = "/";
-                  }}
-                >
-                  <LogoutButton />
-                </Menu.Item>
-              </>
-            )}
-          </Menu.Dropdown>
-        </Menu>
-      </Mobile>
-
-      <Desktop>
-        <Flex
-          className={classes.desktopOptions}
-          align="center"
-          justify="space-between"
-        >
-          <div>
-            {session.data != null &&
-            session.data.session_user.role == UserRole.SystemAdmin ? (
-              <>
-                <AdminPanelButton />
-                <BatchProcessingButton />
-              </>
-            ) : null}
-            <AboutButton />
-          </div>
-          <div>
-            {session.data == null ? <LoginButton /> : <ProfileButton />}
-          </div>
-        </Flex>
-      </Desktop>
-    </>
+    <Flex
+      className={classes.desktopOptions}
+      align="center"
+      justify="space-between"
+    >
+      <div>
+        {session.data != null &&
+        session.data.session_user.role == UserRole.SystemAdmin ? (
+          <>
+            <AdminPanelButton />
+            <BatchProcessingButton />
+          </>
+        ) : null}
+        <AboutButton />
+      </div>
+      <div>{session.data == null ? <LoginButton /> : <ProfileButton />}</div>
+    </Flex>
   );
 }
 
-function AdminPanelButton() {
+const AdminPanelButton = ({ mobile }: { mobile: boolean }) => {
   const router = useRouter();
 
-  return (
-    <>
-      <Mobile>
+  if (mobile) {
+    return (
+      <Menu.Item onClick={() => router.push(Routes.adminPanel)}>
         <Text c="yellow">Admin Panel</Text>
-      </Mobile>
-
-      <Desktop>
-        <Button
-          variant="subtle"
-          color="yellow"
-          onClick={() => router.push(Routes.adminPanel)}
-        >
-          Admin Panel
-        </Button>
-      </Desktop>
-    </>
-  );
-}
-
-function BatchProcessingButton() {
-  const router = useRouter();
+      </Menu.Item>
+    );
+  }
 
   return (
-    <>
-      <Mobile>
+    <Button
+      variant="subtle"
+      color="yellow"
+      onClick={() => router.push(Routes.adminPanel)}
+    >
+      Admin Panel
+    </Button>
+  );
+};
+
+AdminPanelButton.defaultProps = mobileProp;
+
+const BatchProcessingButton = ({ mobile }: { mobile: boolean }) => {
+  const router = useRouter();
+
+  if (mobile) {
+    return (
+      <Menu.Item onClick={() => router.push(Routes.batchProcessing)}>
         <Text c="yellow">Batch Processing</Text>
-      </Mobile>
-
-      <Desktop>
-        <Button
-          variant="subtle"
-          color="yellow"
-          onClick={() => router.push(Routes.batchProcessing)}
-        >
-          Batch Processing
-        </Button>
-      </Desktop>
-    </>
-  );
-}
-
-function AboutButton() {
-  const router = useRouter();
+      </Menu.Item>
+    );
+  }
 
   return (
-    <>
-      <Mobile>
+    <Button
+      variant="subtle"
+      color="yellow"
+      onClick={() => router.push(Routes.batchProcessing)}
+    >
+      Batch Processing
+    </Button>
+  );
+};
+
+BatchProcessingButton.defaultProps = mobileProp;
+
+const AboutButton = ({ mobile }: { mobile: boolean }) => {
+  const router = useRouter();
+
+  if (mobile) {
+    return (
+      <Menu.Item onClick={() => router.push(Routes.about)}>
         <Text c="cyan">About</Text>
-      </Mobile>
-
-      <Desktop>
-        <Button
-          variant="subtle"
-          color="cyan"
-          onClick={() => router.push(Routes.about)}
-        >
-          About
-        </Button>
-      </Desktop>
-    </>
-  );
-}
-
-function LoginButton() {
-  const router = useRouter();
+      </Menu.Item>
+    );
+  }
 
   return (
-    <>
-      <Mobile>
-        <Text c="blue">Login</Text>
-      </Mobile>
-
-      <Desktop>
-        <Button onClick={() => router.push(Routes.login)}>Login</Button>
-      </Desktop>
-    </>
+    <Button
+      variant="subtle"
+      color="cyan"
+      onClick={() => router.push(Routes.about)}
+    >
+      About
+    </Button>
   );
-}
+};
 
-function ProfileButton() {
+AboutButton.defaultProps = mobileProp;
+
+const LoginButton = ({ mobile }: { mobile: boolean }) => {
   const router = useRouter();
+
+  if (mobile) {
+    return (
+      <Menu.Item
+        icon={<IconLogin color="darkblue" />}
+        onClick={() => router.push(Routes.login)}
+      >
+        <Text c="blue">Login</Text>
+      </Menu.Item>
+    );
+  }
+
+  return (
+    <Button color="blue" onClick={() => router.push(Routes.login)}>
+      Login
+    </Button>
+  );
+};
+
+LoginButton.defaultProps = mobileProp;
+
+const ProfileButton = ({ mobile }: { mobile: boolean }) => {
+  const router = useRouter();
+
+  if (mobile) {
+    return (
+      <Menu.Item
+        icon={<IconUserCircle stroke={1} />}
+        onClick={() => router.push(Routes.profile)}
+      >
+        <Text>Your Profile</Text>
+      </Menu.Item>
+    );
+  }
+
+  return (
+    <Menu position="bottom-end" width={150}>
+      <Menu.Target>
+        <ActionIcon size="lg">
+          <IconUserCircle size="4em" />
+        </ActionIcon>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Item
+          icon={<IconUserCircle stroke={1} />}
+          onClick={() => router.push(Routes.profile)}
+        >
+          <Text>Your Profile</Text>
+        </Menu.Item>
+        <LogoutButton />
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
+
+ProfileButton.defaultProps = mobileProp;
+
+const LogoutButton = () => {
   const session = sessionStore();
 
   return (
-    <>
-      <Mobile>
-        <Text>Your Profile</Text>
-      </Mobile>
-
-      <Desktop>
-        <Menu position="bottom-end" width={150}>
-          <Menu.Target>
-            <ActionIcon size="lg">
-              <IconUserCircle size="4em" />
-            </ActionIcon>
-          </Menu.Target>
-
-          <Menu.Dropdown>
-            <Menu.Item
-              icon={<IconUserCircle stroke={1} />}
-              onClick={() => router.push(Routes.profile)}
-            >
-              <Text>Your Profile</Text>
-            </Menu.Item>
-            <Menu.Item
-              icon={<IconLogout color="red" />}
-              onClick={async () => {
-                session.invalidate();
-                await APIClient.logout();
-                location.href = "/";
-              }}
-            >
-              <LogoutButton />
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      </Desktop>
-    </>
+    <Menu.Item
+      icon={<IconLogout color="red" />}
+      onClick={async () => {
+        session.invalidate();
+        await APIClient.logout();
+        location.href = "/";
+      }}
+    >
+      <Text c="red">Logout</Text>
+    </Menu.Item>
   );
-}
-
-function LogoutButton() {
-  return <Text c="red">Logout</Text>;
-}
+};
--- frontend/src/components/responsive.tsx
+++ frontend/src/components/responsive.tsx
@@ -1,23 +1,6 @@
-import { MediaQuery } from "@mantine/core";
+"use client";
+
 import { useMediaQuery } from "@mantine/hooks";
-
-// Shows children in mobile mode.
-export function Mobile({ children }: { children: React.ReactNode }) {
-  return (
-    <MediaQuery largerThan="md" styles={{ display: "none" }}>
-      {children}
-    </MediaQuery>
-  );
-}
-
-// Shows children in desktop mode.
-export function Desktop({ children }: { children: React.ReactNode }) {
-  return (
-    <MediaQuery smallerThan="md" styles={{ display: "none" }}>
-      {children}
-    </MediaQuery>
-  );
-}
 
 // A simple boolean flag to check if the current screen size is suitable for a
 // mobile version or not.
