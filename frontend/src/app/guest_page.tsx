36b6a4f02830de880b575b045fe4ce9dfb16e289
Daryl <aka.hjd@gmail.com>
2023-08-30 11:32:15 +0800

    guest page banner

--- frontend/src/app/guest_page.tsx
+++ frontend/src/app/guest_page.tsx
@@ -1,11 +1,28 @@
 "use client";
 
-import { Center } from "@mantine/core";
+import { Title, createStyles } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+
+const useStyles = createStyles((theme) => ({
+  hero: {
+    padding: "3em 0",
+  }
+}))
 
 export default function GuestHomePage() {
   return (
-    <Center>
-      <p>You are now on the guest home page.</p>
-    </Center>
+    <TopBanner />
   );
+}
+
+function TopBanner() {
+  const {classes} = useStyles()
+
+  const largeScreen = useMediaQuery("(min-width: 62em)")
+
+  return (
+    <div className={classes.hero}>
+      <Title order={largeScreen ? 1 : 2} ta="center">Welcome to the Online Attendance Management System</Title>
+    </div>
+  )
 }
