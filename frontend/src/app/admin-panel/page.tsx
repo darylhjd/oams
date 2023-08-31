b4edd9494df82d8522fed09791bd5528bbf4fd5b
darylhjd <aka.hjd@gmail.com>
2023-08-31 09:30:38 +0800

    change

--- frontend/src/app/admin-panel/page.tsx
+++ frontend/src/app/admin-panel/page.tsx
@@ -6,22 +6,20 @@ import { Routes } from "@/routes/routes";
 import { sessionStore } from "@/states/session";
 import { Center } from "@mantine/core";
 import { getURL } from "next/dist/shared/lib/utils";
-import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function AdminPanelPage() {
-  const router = useRouter();
   const session = sessionStore();
 
   useEffect(() => {
     if (session.data == null) {
-      router.replace(
+      location.replace(
         `${Routes.login}?${buildRedirectUrlQueryParamsString(getURL())}`,
       );
     } else if (session.data!.session_user.role != UserRole.SystemAdmin) {
-      router.replace(Routes.home);
+      location.replace(Routes.home);
     }
-  }, [router, session]);
+  }, [session]);
 
   if (
     session.data == null ||
