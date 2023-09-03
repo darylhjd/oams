0a38b9c0ab832fc41df0d48576371d6f1f6c4458
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-09-03 20:51:11 +0800

    feat: add information on about page (#182)

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { Center } from "@mantine/core";
+import { Container, Text } from "@mantine/core";
 
 export default function AboutPage() {
   return (
-    <Center>
-      <p>This is the about page.</p>
-    </Center>
+    <Container>
+      <Text align="center">
+        This application is created as part of the Final Year Paper requirement
+        for the Bachelor of Engineering in Computer Science in Nanyang
+        Technological University.
+      </Text>
+      <br />
+      <Text align="center">Copyright &#169; Har Jing Daryl, 2023</Text>
+    </Container>
   );
 }
