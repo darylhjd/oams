e3a5a06db406216b81ab781a911b4513ebe68692
darylhjd <aka.hjd@gmail.com>
2023-09-07 18:38:02 +0800

    all tables

--- frontend/src/app/batch-processing/batch_tables.tsx
+++ frontend/src/app/batch-processing/batch_tables.tsx
@@ -3,17 +3,6 @@ import { ClassGroupData, UpsertClassParams } from "@/api/models";
 import { DataTable } from "mantine-datatable";
 
 export function ClassesTable({ cls }: { cls: UpsertClassParams }) {
-  const row = (
-    <tr key={cls.code + cls.year.toString() + cls.semester}>
-      <td>0</td>
-      <td>{cls.code}</td>
-      <td>{cls.year}</td>
-      <td>{cls.semester}</td>
-      <td>{cls.programme}</td>
-      <td>{cls.au}</td>
-    </tr>
-  );
-
   return (
     <DataTable
       withBorder
@@ -82,51 +71,34 @@ export function ClassGroupSessionsTable({
 
       return group.sessions;
     })
-    .flat()
-    .map((session) => (
-      <tr key={session.class_group_id + session.start_time.toString()}>
-        <td>{session.class_group_id}</td>
-        <td>{session.start_time.toString()}</td>
-        <td>{session.end_time.toString()}</td>
-        <td>{session.venue}</td>
-      </tr>
-    ));
+    .flat();
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>Class Group ID</th>
-          <th>Start Time</th>
-          <th>End Time</th>
-          <th>Venue</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+    <DataTable
+      withBorder
+      withColumnBorders
+      highlightOnHover
+      columns={[
+        { accessor: "class_group_id", title: "Class Group ID" },
+        { accessor: "start_time", title: "Start Time" },
+        { accessor: "end_time", title: "End Time" },
+        { accessor: "venue" },
+      ]}
+      records={rows}
+    />
   );
 }
 
 export function UsersTable({ classGroups }: { classGroups: ClassGroupData[] }) {
-  const rows = classGroups
-    .map((group) => group.students)
-    .flat()
-    .map((student) => (
-      <tr key={student.id}>
-        <td>{student.id}</td>
-        <td>{student.name}</td>
-      </tr>
-    ));
+  const rows = classGroups.map((group) => group.students).flat();
 
   return (
-    <Table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </Table>
+    <DataTable
+      withBorder
+      withColumnBorders
+      highlightOnHover
+      columns={[{ accessor: "id", title: "ID" }, { accessor: "name" }]}
+      records={rows}
+    />
   );
 }
