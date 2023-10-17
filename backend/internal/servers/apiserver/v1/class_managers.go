363b462fba00bf8c18f2396e0058a67dcec94290
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-10-17 17:00:27 +0800

    feat(backend/apiserver): implement class managers get endpoint (#210)

--- backend/internal/servers/apiserver/v1/class_managers.go
+++ backend/internal/servers/apiserver/v1/class_managers.go
@@ -2,6 +2,9 @@ package v1
 
 import (
 	"net/http"
+
+	"github.com/darylhjd/oams/backend/internal/database/gen/oams/public/model"
+	"github.com/darylhjd/oams/backend/internal/database/gen/oams/public/table"
 )
 
 func (v *APIServerV1) classManagers(w http.ResponseWriter, r *http.Request) {
@@ -9,7 +12,7 @@ func (v *APIServerV1) classManagers(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodGet:
-		resp = newErrorResponse(http.StatusNotImplemented, "")
+		resp = v.classManagersGet(r)
 	case http.MethodPost:
 		resp = newErrorResponse(http.StatusNotImplemented, "")
 	default:
@@ -17,4 +20,30 @@ func (v *APIServerV1) classManagers(w http.ResponseWriter, r *http.Request) {
 	}
 
 	v.writeResponse(w, r, resp)
+}
+
+type classManagersGetResponse struct {
+	response
+	ClassManagers []model.ClassManager `json:"class_managers"`
+}
+
+func (v *APIServerV1) classManagersGet(r *http.Request) apiResponse {
+	params, err := v.decodeListQueryParameters(r.URL.Query(), table.ClassManagers.AllColumns)
+	if err != nil {
+		return newErrorResponse(http.StatusBadRequest, err.Error())
+	}
+
+	managers, err := v.db.ListClassManagers(r.Context(), params)
+	if err != nil {
+		v.logInternalServerError(r, err)
+		return newErrorResponse(http.StatusInternalServerError, "could not process class managers get database action")
+	}
+
+	resp := classManagersGetResponse{
+		newSuccessResponse(),
+		make([]model.ClassManager, 0, len(managers)),
+	}
+
+	resp.ClassManagers = append(resp.ClassManagers, managers...)
+	return resp
 }
--- backend/internal/servers/apiserver/v1/class_managers_test.go
+++ backend/internal/servers/apiserver/v1/class_managers_test.go
@@ -1,10 +1,12 @@
 package v1
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/darylhjd/oams/backend/internal/database/gen/oams/public/model"
 	"github.com/darylhjd/oams/backend/internal/tests"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -21,7 +23,7 @@ func TestAPIServerV1_classManagers(t *testing.T) {
 		{
 			"with GET method",
 			http.MethodGet,
-			http.StatusNotImplemented,
+			http.StatusOK,
 		},
 		{
 			"with POST method",
@@ -51,6 +53,67 @@ func TestAPIServerV1_classManagers(t *testing.T) {
 			v1.classManagers(rr, req)
 
 			a.Equal(tt.wantStatusCode, rr.Code)
+		})
+	}
+}
+
+func TestAPIServerV1_classManagersGet(t *testing.T) {
+	t.Parallel()
+
+	tts := []struct {
+		name                     string
+		withExistingClassManager bool
+		wantResponse             classManagersGetResponse
+	}{
+		{
+			"request with class manager in database",
+			true,
+			classManagersGetResponse{
+				newSuccessResponse(),
+				[]model.ClassManager{
+					{
+						ManagingRole: model.ManagingRole_CourseCoordinator,
+					},
+				},
+			},
+		},
+		{
+			"request with no class manager in database",
+			false,
+			classManagersGetResponse{
+				newSuccessResponse(),
+				[]model.ClassManager{},
+			},
+		},
+	}
+
+	for _, tt := range tts {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			a := assert.New(t)
+			ctx := context.Background()
+			id := uuid.NewString()
+
+			v1 := newTestAPIServerV1(t, id)
+			defer tests.TearDown(t, v1.db, id)
+
+			if tt.withExistingClassManager {
+				for idx, manager := range tt.wantResponse.ClassManagers {
+					createdManager := tests.StubClassManager(t, ctx, v1.db, manager.ManagingRole)
+					managerPtr := &tt.wantResponse.ClassManagers[idx]
+					managerPtr.ID = createdManager.ID
+					managerPtr.UserID = createdManager.UserID
+					managerPtr.ClassID = createdManager.ClassID
+					managerPtr.CreatedAt, managerPtr.UpdatedAt = createdManager.CreatedAt, createdManager.CreatedAt
+				}
+			}
+
+			req := httptest.NewRequest(http.MethodGet, classManagersUrl, nil)
+			actualResp, ok := v1.classManagersGet(req).(classManagersGetResponse)
+			a.True(ok)
+			a.Equal(tt.wantResponse, actualResp)
 		})
 	}
 }
