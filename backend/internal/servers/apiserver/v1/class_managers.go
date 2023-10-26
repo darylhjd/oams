02e0921cd4880544fb103f8a8236f7adf4e23ddd
Har Jing Daryl <53652695+darylhjd@users.noreply.github.com>
2023-10-26 17:42:47 +0800

    feat(backend/apiserver): implement class managers post request (#213)

--- backend/internal/servers/apiserver/v1/class_managers.go
+++ backend/internal/servers/apiserver/v1/class_managers.go
@@ -1,8 +1,11 @@
 package v1
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/darylhjd/oams/backend/internal/database"
 	"github.com/darylhjd/oams/backend/internal/database/gen/oams/public/model"
 	"github.com/darylhjd/oams/backend/internal/database/gen/oams/public/table"
 )
@@ -14,7 +17,7 @@ func (v *APIServerV1) classManagers(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		resp = v.classManagersGet(r)
 	case http.MethodPost:
-		resp = newErrorResponse(http.StatusNotImplemented, "")
+		resp = v.classManagersPost(r)
 	default:
 		resp = newErrorResponse(http.StatusMethodNotAllowed, "")
 	}
@@ -46,4 +49,52 @@ func (v *APIServerV1) classManagersGet(r *http.Request) apiResponse {
 
 	resp.ClassManagers = append(resp.ClassManagers, managers...)
 	return resp
+}
+
+type classManagersPostRequest struct {
+	ClassManager database.CreateClassManagerParams `json:"class_manager"`
+}
+
+type classManagersPostResponse struct {
+	response
+	ClassManager classManagersPostClassManagerResponseFields `json:"class_manager"`
+}
+
+type classManagersPostClassManagerResponseFields struct {
+	ID           int64              `json:"id"`
+	UserID       string             `json:"user_id"`
+	ClassID      int64              `json:"class_id"`
+	ManagingRole model.ManagingRole `json:"managing_role"`
+	CreatedAt    time.Time          `json:"created_at"`
+}
+
+func (v *APIServerV1) classManagersPost(r *http.Request) apiResponse {
+	var req classManagersPostRequest
+	if err := v.parseRequestBody(r.Body, &req); err != nil {
+		return newErrorResponse(http.StatusBadRequest, fmt.Sprintf("could not parse request body: %s", err))
+	}
+
+	manager, err := v.db.CreateClassManager(r.Context(), req.ClassManager)
+	if err != nil {
+		switch {
+		case database.ErrSQLState(err, database.SQLStateDuplicateKeyOrIndex):
+			return newErrorResponse(http.StatusConflict, "class manager with same user_id and class_id already exists")
+		case database.ErrSQLState(err, database.SQLStateForeignKeyViolation):
+			return newErrorResponse(http.StatusBadRequest, "user_id and/or class_id does not exist")
+		default:
+			v.logInternalServerError(r, err)
+			return newErrorResponse(http.StatusInternalServerError, "could not process class managers post database action")
+		}
+	}
+
+	return classManagersPostResponse{
+		newSuccessResponse(),
+		classManagersPostClassManagerResponseFields{
+			manager.ID,
+			manager.UserID,
+			manager.ClassID,
+			manager.ManagingRole,
+			manager.CreatedAt,
+		},
+	}
 }
