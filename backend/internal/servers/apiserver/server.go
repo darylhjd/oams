387013c729c57a409adc55bd1caf4c11bf58f187
darylhjd <aka.hjd@gmail.com>
2023-10-27 13:56:04 +0800

    agre

--- backend/internal/servers/apiserver/server.go
+++ backend/internal/servers/apiserver/server.go
@@ -62,6 +62,7 @@ func (s *APIServer) Start() error {
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{fmt.Sprintf("%s", env.GetWebServerHost())},
 		AllowCredentials: true,
+		AllowedHeaders:   []string{"Set-Cookie"},
 		Debug:            true,
 	})
 
