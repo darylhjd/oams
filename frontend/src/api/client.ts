afcc017efccae80f5a0113680312ab1d20957301
darylhjd <aka.hjd@gmail.com>
2023-10-28 20:55:37 +0800

    apiclient endpoints

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { LoginResponse } from "./login";
-import { UserMeResponse } from "./user";
+import { UserMeResponse, UsersGetResponse } from "./user";
 import { BatchData, BatchPostResponse, BatchPutResponse } from "./batch";
 import { FileWithPath } from "@mantine/dropzone";
+import { ClassesGetResponse } from "./class";
+import { ClassManagersGetResponse } from "./class_manager";
+import { ClassGroupsGetResponse } from "./class_group";
+import { ClassGroupSessionsGetResponse } from "./class_group_session";
+import { SessionEnrollmentsGetResponse } from "./session_enrollment";
 
 export class APIClient {
   static _client = axios.create({
@@ -13,7 +18,13 @@ export class APIClient {
   static readonly _loginPath = "/login";
   static readonly _logoutPath = "/logout";
   static readonly _batchPath = "/batch";
+  static readonly _usersPath = "/users";
   static readonly _userMePath = "/users/me";
+  static readonly _classesPath = "/classes";
+  static readonly _classManagersPath = "/class-managers";
+  static readonly _classGroupsPath = "/class-groups";
+  static readonly _classGroupSessionsPath = "/class-group-sessions";
+  static readonly _sessionEnrollmentsPath = "/session-enrollments";
 
   static async login(redirectUrl: string = ""): Promise<string> {
     const { data } = await this._client.get<LoginResponse>(this._loginPath, {
@@ -47,8 +58,101 @@ export class APIClient {
     return data;
   }
 
+  static async usersGet(
+    offset: number,
+    limit: number,
+  ): Promise<UsersGetResponse> {
+    const { data } = await this._client.get<UsersGetResponse>(this._usersPath, {
+      params: {
+        offset: offset,
+        limit: limit,
+      },
+    });
+    return data;
+  }
+
   static async userMe(): Promise<UserMeResponse> {
     const { data } = await this._client.get<UserMeResponse>(this._userMePath);
+    return data;
+  }
+
+  static async classesGet(
+    offset: number,
+    limit: number,
+  ): Promise<ClassesGetResponse> {
+    const { data } = await this._client.get<ClassesGetResponse>(
+      this._classesPath,
+      {
+        params: {
+          offset: offset,
+          limit: limit,
+        },
+      },
+    );
+    return data;
+  }
+
+  static async classManagersGet(
+    offset: number,
+    limit: number,
+  ): Promise<ClassManagersGetResponse> {
+    const { data } = await this._client.get<ClassManagersGetResponse>(
+      this._classManagersPath,
+      {
+        params: {
+          offset: offset,
+          limit: limit,
+        },
+      },
+    );
+    return data;
+  }
+
+  static async classGroupsGet(
+    offset: number,
+    limit: number,
+  ): Promise<ClassGroupsGetResponse> {
+    const { data } = await this._client.get<ClassGroupsGetResponse>(
+      this._classGroupsPath,
+      {
+        params: {
+          offset: offset,
+          limit: limit,
+        },
+      },
+    );
+    return data;
+  }
+
+  static async classGroupSessionsGet(
+    offset: number,
+    limit: number,
+  ): Promise<ClassGroupSessionsGetResponse> {
+    const { data } = await this._client.get<ClassGroupSessionsGetResponse>(
+      this._classGroupSessionsPath,
+      {
+        params: {
+          offset: offset,
+          limit: limit,
+        },
+      },
+    );
+    return data;
+  }
+
+  static async sessionEnrollmentsGet(
+    offset: number,
+    limit: number,
+  ): Promise<SessionEnrollmentsGetResponse> {
+    const { data } = await this._client.get<SessionEnrollmentsGetResponse>(
+      this._sessionEnrollmentsPath,
+      {
+        params: {
+          offset: offset,
+          limit: limit,
+        },
+      },
+    );
     return data;
   }
 }
