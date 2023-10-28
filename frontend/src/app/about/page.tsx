11d0c6859c4156df6a08feacabfc149c3ec31b42
darylhjd <aka.hjd@gmail.com>
2023-10-28 12:02:18 +0800

    about page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -1,9 +1,17 @@
-import { Center } from "@mantine/core";
+import { Space, Text } from "@mantine/core";
+import styles from "@/styles/AboutPage.module.css";
 
 export default function AboutPage() {
   return (
-    <Center>
-      <p>This is the about page.</p>
-    </Center>
+    <>
+      <Text ta="center" className={styles.aboutText}>
+        The Online Attendance Management System provides a centralised
+        attendance service for Nanyang Technological University.
+      </Text>
+      <Space h="xl" />
+      <Text ta="center" size="sm">
+        Copyright &#169; Har Jing Daryl, 2023
+      </Text>
+    </>
   );
 }
