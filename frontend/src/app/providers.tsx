3b5a63e9c3ccc6a099600a22ca9df07b79f51b90
darylhjd <aka.hjd@gmail.com>
2023-10-29 13:20:01 +0800

    session initialiser move

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,6 +1,13 @@
-import { SessionInitialiser } from "@/stores/session";
-import { MantineProvider } from "@mantine/core";
+"use client";
+
+import styles from "@/styles/Providers.module.css";
+
+import { APIClient } from "@/api/client";
+import { Center, MantineProvider, Skeleton } from "@mantine/core";
 import { Notifications } from "@mantine/notifications";
+import { motion } from "framer-motion";
+import { useSessionUserStore } from "@/stores/session";
+import { useEffect, useState } from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -8,5 +15,39 @@ export default function Providers({ children }: { children: React.ReactNode }) {
       <Notifications />
       <SessionInitialiser>{children}</SessionInitialiser>
     </MantineProvider>
+  );
+}
+
+function SessionInitialiser({ children }: { children: React.ReactNode }) {
+  const session = useSessionUserStore();
+  const [loaded, setLoaded] = useState(false);
+  const [fetching, setFetching] = useState(false);
+
+  useEffect(() => {
+    if (fetching) {
+      return;
+    }
+
+    setFetching(true);
+    APIClient.userMe()
+      .then((data) => {
+        session.setSession(data);
+      })
+      .catch((_error) => null)
+      .finally(() => setLoaded(true));
+  }, [fetching, session]);
+
+  return loaded ? (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
+      {children}
+    </motion.div>
+  ) : (
+    <Center className={styles.center}>
+      <Skeleton className={styles.skeleton} visible={true} />
+    </Center>
   );
 }
