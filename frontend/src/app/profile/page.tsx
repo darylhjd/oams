4da1b29c838a20369e01201225cbb8a113dfeb00
darylhjd <aka.hjd@gmail.com>
2023-10-29 16:29:36 +0800

    profile page

--- frontend/src/app/profile/page.tsx
+++ frontend/src/app/profile/page.tsx
@@ -1,10 +1,27 @@
 "use client";
 
+import styles from "@/styles/ProfilePage.module.css";
+
 import { useSessionUserStore } from "@/stores/session";
-import { Text } from "@mantine/core";
+import { Paper, Space, Text } from "@mantine/core";
 
 export default function ProfilePage() {
   const session = useSessionUserStore();
+  const user = session.data!.session_user;
 
-  return <Text ta="center">{session.data?.session_user.name}</Text>;
+  return (
+    <Paper className={styles.paper} radius="md" shadow="xs" withBorder p="xl">
+      <Text ta="center" size="xl" fw={1000}>
+        {user.id}
+      </Text>
+      <Space h="md" />
+      <Text ta="center" size="sm">
+        {user.role} • {user.email}
+      </Text>
+      <Space h="xs" />
+      <Text c="dimmed" fs="italic" ta="center" size="sm">
+        {user.name ? user.name : "No name registed"}
+      </Text>
+    </Paper>
+  );
 }
