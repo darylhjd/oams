30f8e8815f7a8d376aee247e532eaa6d8127c580
darylhjd <aka.hjd@gmail.com>
2023-10-29 22:25:08 +0800

    use effect deps

--- frontend/src/app/admin-panel/entity_loader.tsx
+++ frontend/src/app/admin-panel/entity_loader.tsx
@@ -25,7 +25,7 @@ export function EntityLoader({
     promiseFunc()
       .catch((error) => setError(error))
       .finally(() => setLoaded(true));
-  });
+  }, [fetching, promiseFunc]);
 
   if (!loaded) {
     return (
