5d2caca7c91848ead5eafd4bd4129b190275576b
darylhjd <aka.hjd@gmail.com>
2023-11-28 12:06:54 +0800

    chore(frontend): set default logged page style

--- frontend/src/app/page_logged.tsx
+++ frontend/src/app/page_logged.tsx
@@ -1,14 +1,16 @@
 "use client";
 
+import styles from "@/styles/PageLogged.module.css";
+
 import { useSessionUserStore } from "@/stores/session";
-import { Center } from "@mantine/core";
+import { Title } from "@mantine/core";
 
 export default function LoggedPage() {
   const session = useSessionUserStore();
 
   return (
-    <Center>
-      <p>Hello there, {session.data?.session_user.name}</p>
-    </Center>
+    <Title order={2} ta="center" className={styles.title}>
+      Welcome, {session.data!.session_user.name}
+    </Title>
   );
 }
