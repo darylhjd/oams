8eb4b0790ef0b4060ae0e0b0e3d816f8591f6778
darylhjd <aka.hjd@gmail.com>
2023-12-21 11:12:36 +0800

    feat: include more fields for upcoming class group session

--- backend/internal/database/attendance_taking.go
+++ backend/internal/database/attendance_taking.go
@@ -13,11 +13,25 @@ const (
 	attendanceTimeBuffer = time.Minute * 15
 )
 
-func (d *DB) GetUpcomingManagedClassGroupSessions(ctx context.Context) ([]model.ClassGroupSession, error) {
-	var res []model.ClassGroupSession
+type UpcomingManagedClassGroupSession struct {
+	ID           int64               `alias:"class_group_session.id"`
+	StartTime    time.Time           `alias:"class_group_session.start_time"`
+	EndTime      time.Time           `alias:"class_group_session.end_time"`
+	Venue        string              `alias:"class_group_session.venue"`
+	ClassType    model.ClassType     `alias:"class_group.class_type"`
+	ManagingRole *model.ManagingRole `alias:"class_group_manager.managing_role"`
+}
+
+func (d *DB) GetUpcomingManagedClassGroupSessions(ctx context.Context) ([]UpcomingManagedClassGroupSession, error) {
+	var res []UpcomingManagedClassGroupSession
 
 	stmt := SELECT(
-		ClassGroupSessions.AllColumns,
+		ClassGroupSessions.ID,
+		ClassGroupSessions.StartTime,
+		ClassGroupSessions.EndTime,
+		ClassGroupSessions.Venue,
+		ClassGroups.ClassType,
+		ClassGroupManagers.ManagingRole,
 	).FROM(
 		ClassGroupSessions.INNER_JOIN(
 			ClassGroups, ClassGroups.ID.EQ(ClassGroupSessions.ClassGroupID),
--- backend/internal/servers/apiserver/v1/attendance_taking.go
+++ backend/internal/servers/apiserver/v1/attendance_taking.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/darylhjd/oams/backend/internal/database/gen/postgres/public/model"
 )
@@ -21,14 +22,22 @@ func (v *APIServerV1) attendanceTaking(w http.ResponseWriter, r *http.Request) {
 
 type attendanceTakingGetResponse struct {
 	response
-	UpcomingClassGroupSessions []model.ClassGroupSession `json:"upcoming_class_group_sessions"`
+	UpcomingClassGroupSessions []attendanceTakingGetUpcomingClassGroupSessionResponseFields `json:"upcoming_class_group_sessions"`
+}
+
+type attendanceTakingGetUpcomingClassGroupSessionResponseFields struct {
+	ID           int64               `json:"id"`
+	StartTime    time.Time           `json:"start_time"`
+	EndTime      time.Time           `json:"end_time"`
+	Venue        string              `json:"venue"`
+	ClassType    model.ClassType     `json:"class_type"`
+	ManagingRole *model.ManagingRole `json:"managing_role"` // For nil values, exposed as system admin.
 }
 
 // TODO: Implement tests for this endpoint.
 func (v *APIServerV1) attendanceTakingGet(r *http.Request) apiResponse {
 	resp := attendanceTakingGetResponse{
-		response:                   newSuccessResponse(),
-		UpcomingClassGroupSessions: []model.ClassGroupSession{},
+		response: newSuccessResponse(),
 	}
 
 	upcoming, err := v.db.GetUpcomingManagedClassGroupSessions(r.Context())
@@ -37,6 +46,17 @@ func (v *APIServerV1) attendanceTakingGet(r *http.Request) apiResponse {
 		return newErrorResponse(http.StatusInternalServerError, "could not get upcoming managed class group sessions")
 	}
 
-	resp.UpcomingClassGroupSessions = append(resp.UpcomingClassGroupSessions, upcoming...)
+	resp.UpcomingClassGroupSessions = make([]attendanceTakingGetUpcomingClassGroupSessionResponseFields, 0, len(upcoming))
+	for _, m := range upcoming {
+		resp.UpcomingClassGroupSessions = append(resp.UpcomingClassGroupSessions, attendanceTakingGetUpcomingClassGroupSessionResponseFields{
+			m.ID,
+			m.StartTime,
+			m.EndTime,
+			m.Venue,
+			m.ClassType,
+			m.ManagingRole,
+		})
+	}
+
 	return resp
 }
