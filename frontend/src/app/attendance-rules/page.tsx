411de79c9734825ec2e1937338f2c870d8134924
darylhjd <aka.hjd@gmail.com>
2023-12-26 19:59:56 +0800

    feat(frontend): implement attendance rules skeleton page

---
+++ frontend/src/app/attendance-rules/page.tsx
@@ -0,0 +1,19 @@
+import styles from "@/styles/AttendanceRules.module.css";
+
+import { Container, Text, Title } from "@mantine/core";
+
+export default function AttendanceRulesPage() {
+  return (
+    <Container className={styles.container} fluid>
+      <PageTitle />
+    </Container>
+  );
+}
+
+function PageTitle() {
+  return (
+    <Title order={3} ta="center">
+      Attendance Rules
+    </Title>
+  );
+}
