6ffbc93faa916d5af4f7cea32ee1ecd101cbddca
darylhjd <aka.hjd@gmail.com>
2024-01-01 01:24:52 +0800

    save

--- backend/cmd/intervention/main.go
+++ backend/cmd/intervention/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/darylhjd/oams/backend/internal/database"
+	"github.com/darylhjd/oams/backend/internal/logger"
+	"go.uber.org/zap"
+)
+
+const (
+	namespace = "intervention"
+)
 
 func main() {
-	fmt.Println("Function was run")
+	ctx := context.Background()
+
+	lg, err := logger.NewLogger()
+	if err != nil {
+		log.Fatalf("%s - failed to initialise logger: %s", namespace, err)
+	}
+
+	db, err := database.Connect(ctx)
+	if err != nil {
+		lg.Fatal(fmt.Sprintf("%s - could not connect to database", namespace), zap.Error(err))
+	}
+	defer func() {
+		if err = db.Close(); err != nil {
+			lg.Fatal(fmt.Sprintf("%s - could not close database connection", namespace), zap.Error(err))
+		}
+	}()
 }
