845af1adfcf68711a0db783d0ec116585d87ba32
darylhjd <aka.hjd@gmail.com>
2024-01-09 15:33:30 +0800

    save backend

--- backend/internal/servers/apiserver/v1/coordinating_class_dashboard.go
+++ backend/internal/servers/apiserver/v1/coordinating_class_dashboard.go
@@ -2,6 +2,8 @@ package v1
 
 import (
 	"net/http"
+
+	"github.com/darylhjd/oams/backend/internal/database"
 )
 
 func (v *APIServerV1) coordinatingClassDashboard(w http.ResponseWriter, r *http.Request, classId int64) {
@@ -19,10 +21,17 @@ func (v *APIServerV1) coordinatingClassDashboard(w http.ResponseWriter, r *http.
 
 type coordinatingClassDashboardGetResponse struct {
 	response
+	AttendanceCount []database.AttendanceCountData `json:"attendance_count"`
 }
 
-func (v *APIServerV1) coordinatingClassDashboardGet(_ *http.Request, _ int64) apiResponse {
+func (v *APIServerV1) coordinatingClassDashboardGet(r *http.Request, classId int64) apiResponse {
+	data, err := v.db.GetDashboardData(r.Context(), classId)
+	if err != nil {
+		return newErrorResponse(http.StatusInternalServerError, "could not get dashboard data")
+	}
+
 	return coordinatingClassDashboardGetResponse{
 		newSuccessResponse(),
+		append(make([]database.AttendanceCountData, 0, len(data)), data...),
 	}
 }
